ba3858a2df4b449eec5e823a70630980485be294
96RIck <lesterblack@yahoo.com>
2017-10-17 13:52:36 +0800

    Project-03 first commit

---
+++ Project-03 LocalVideo/Project-03 LocalVideo/VideoCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCell.swift
+//  Project-03 LocalVideo
+//
+//  Created by Rick on 2017/10/17.
+//  Copyright © 2017年 Rick. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Project-03 LocalVideo/Project-03 LocalVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project-03 LocalVideo
+//
+//  Created by Rick on 2017/10/17.
+//  Copyright © 2017年 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
